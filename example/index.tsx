bc20a5a07d591ce295fff66a23cb82be72418d39
Lior Pollak <4294489+liorp@users.noreply.github.com>
2022-02-04 18:46:57 +0200

    fix: maybe

--- example/index.tsx
+++ example/index.tsx
@@ -31,7 +31,7 @@ const App = () => {
     <HotkeysDocsContext.Provider value={{ hotkeysDocs, setHotkeysDocs }}>
       <div>
         {Object.keys(hotkeysDocs).map(section => (
-          <div>
+          <div key={section}>
             <h2>{section}</h2>
             {hotkeysDocs[section].map(k => (
               <span>
